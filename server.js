84bc060b581796d508cf2973723408af7e791855
kyle <nojaim.k@gmail.com>
2025-01-06 17:43:18 -0500

    basic setup created

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3001;
+
+let phonebook = [
+    {
+        id: '1',
+        name: 'Mario',
+        number: '415-636-9090'
+    },
+    {
+        id: '2',
+        name: 'Luigi',
+        number: '1-744-620-1444'
+    },
+    {
+        id: '3',
+        name: 'Yoshi',
+        number: '822-438-9967'
+    },
+    {
+        id: '4',
+        name: 'Donkey Kong',
+        number: '904-901-9131'
+    },
+]
+
+
+app.listen(port, () => {
+    console.log(`server listening on port:${port}`)
+})
